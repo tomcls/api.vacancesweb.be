8ebc2e55c830979079fa6042d8ac6a756f39e1d0
Thomas Claessens <tom@itcl.io>
2023-02-25 15:59:49 +0100

    ...

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('firstname',60);
+            $table->string('lastname',60);
+            $table->string('lang',2);
+            $table->string('phone',40);
+            $table->string('company_name',60);
+            $table->string('company_vat',25);
+            $table->boolean('active')->default(false);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
